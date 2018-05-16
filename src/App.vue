f6f371189397b496d5a6c9d83831b10dc9a257fa
wangxia <1901399939@qq.com>
2018-05-16 13:21:28 +0800

    h

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
   name: 'App',
-  data() {
+  data () {
     return {
       message: 'hello hometown!'
     }
@@ -15,13 +15,34 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    font-size: 100%;
+    overflow: hidden;
+    background: #f5c4c6;
+  }
+
+  ul,
+  ol,
+  li {
+    list-style: none;
+  }
+
+  a {
+    -webkit-tap-highlight-color: rgba(255, 0, 0, 0);
+    text-decoration: none;
+
+    &:hover {
+      color: inherit;
+    }
+  }
+
+  #app{
+    background: url('./assets/images/bg3.jpg') no-repeat top center;
+    background-size: cover;
+  }
 </style>
